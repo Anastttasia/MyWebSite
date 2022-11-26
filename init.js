ce22e855fc0e893c39c107fedc6e8754a4f52e60
Anastttasia <nastya.kaneva.99@gmail.com>
2022-11-26 18:03:13 +0300

    finish

--- init.js
+++ init.js
@@ -1,3 +1,27 @@
+
+let clock = document.querySelector('.nowTime');
+
+function time() {
+    let nowDate = new Date();
+    let hours = nowDate.getHours();
+    let minutes = nowDate.getMinutes();
+    let sec = nowDate.getSeconds();
+
+    if(hours < 10) {
+        hours = "0" + hours;
+    }
+    if(minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if(sec < 10) {
+        sec = "0" + sec;
+    }
+    
+    clock.innerHTML = `${hours}:${minutes}:${sec}`;
+}
+setInterval(time, 1000);
+
+
 function closeAllModals(event) {
     
     if(event.type == "keydown") {
@@ -30,8 +54,6 @@ function openModalWindow(imgBlock){
     document.addEventListener("keydown", closeAllModals);
     img.addEventListener("click", closeAllModals);
 }
-
-
 
 
 
